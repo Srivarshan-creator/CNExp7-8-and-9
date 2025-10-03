38d9e91f7ecf746fa7120ede0258fe15be597a06
Srivarshan-creator <139067989+Srivarshan-creator@users.noreply.github.com>
2025-10-03 17:00:05 +0530

    Add files via upload

---
+++ install_ns.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install dependencies
+sudo apt-get update && sudo apt-get install -y build-essential autoconf automake libxmu-dev gcc git
+
+# Clone NS2 repository
+git clone https://github.com/idmidr/ns-allinone-2.35.git
+
+# Install NS2
+cd ns-allinone-2.35/ || exit
+./install
+
+# Set environment variables in ~/.bashrc
+echo -e "\n# NS2 environment variables" >> ~/.bashrc
+echo 'export PATH=$PATH:~/ns-allinone-2.35/bin:~/ns-allinone-2.35/tcl8.5.10/unix:~/ns-allinone-2.35/tk8.5.10/unix' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/ns-allinone-2.35/otcl-1.14:~/ns-allinone-2.35/lib' >> ~/.bashrc
+echo 'export TCL_LIBRARY=$TCL_LIB:~/ns-allinone-2.35/tcl8.5.10/library:/usr/lib' >> ~/.bashrc
+
+# Apply changes
+source ~/.bashrc
+
+echo "NS2 installation complete. You can now run 'ns'."
+
